f0d5b9137ccd4983167f8a8f3e4cc58842a2b011
ooukd <oukd@my.easternct.edu>
2024-10-31 22:02:50 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,16 +2,16 @@
 
 public class hello {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-       // initialize variables
-       String firstName = "Alex";
+        // initialize variables
+        String firstName = "Darick";
 
-       // delete this line
-       System.out.println("Modify this code");
-	
-       // Using the value of the variable above, output a message in the format, "Hello, I am Alex".
+        // delete this line
+        System.out.println("Hello, I am " + firstName);
+
+        // Using the value of the variable above, output a message in the format,
+        // "Hello, I am Alex".
 
     }
 }
-
