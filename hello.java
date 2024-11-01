1abe32eababe921d12fcfa5100aba92a2a72f9fc
ooukd <oukd@my.easternct.edu>
2024-10-31 22:04:31 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@ public class hello {
         String firstName = "Darick";
 
         // delete this line
-        System.out.println("Hello, I am " + firstName);
+        System.out.println("Hello, I am " + firstName + " ");
 
         // Using the value of the variable above, output a message in the format,
         // "Hello, I am Alex".
